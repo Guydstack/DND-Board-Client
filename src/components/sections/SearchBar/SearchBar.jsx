87bbe5f49ec74e276514951848a176a664e5eb09
Guydstack <129586924+Guydstack@users.noreply.github.com>
2024-05-29 12:15:26 +0300

    Update SearchBar.jsx

--- src/components/sections/SearchBar/SearchBar.jsx
+++ src/components/sections/SearchBar/SearchBar.jsx
@@ -6,92 +6,6 @@
   import { AuthContext } from "../../../context/AuthContext";
   import ProductCard from "../../partials/products/ProductCard";
 
-  // import "../../../../src/components/sections/SearchBar/SearchBar-Chakra.css"
-
-  // const SearchBar = () =>{
-  //   const { isOpen, onOpen, onClose } = useDisclosure()
-  //   const {searchTerm, setSearchTerm} = useContext(AuthContext);
-  
-
-  //   function updateMaxHeight (newValue){
-  //     if (newValue === 1) {    
-  //       document.documentElement.style.setProperty('--max-height', '150px');
-  //     } else 
-  //     document.documentElement.style.setProperty('--max-height', 'auto');
-  //   };
-
-
-
-  
-  //   return (
-  //     <>
-  //       <AiOutlineSearch color={"black"} size={25} onClick={() => { onOpen(); updateMaxHeight(1); }} style={{ cursor: "pointer" }} />
-  
-  //       <Modal isOpen={isOpen} onClose={onClose} blockScrollOnMount={false} closeOnOverlayClick={false}>
-  //         <ModalContent display="flex" alignItems="center" justifyContent="center">
-  //           <InputGroup>
-  //           <Input
-  //             placeholder="Enter Something..."
-  //             _placeholder={{ color: "black" }}
-  //             onChange={(e) => setSearchTerm(e.target.value)}
-  //             value={searchTerm}
-  //           />
-  //             <InputLeftElement>
-  //               <SearchIcon />
-  //             </InputLeftElement>
-  //             <InputRightElement>
-  //               <ModalCloseButton top={1} onClick={() => { setSearchTerm(""); updateMaxHeight(2); }}/>
-  //             </InputRightElement>
-  //           </InputGroup>
-  //         </ModalContent>
-  //       </Modal>
-  //     </>
-  //   )
-  // };
-
-
-  // const SearchBar = () => {
-    
-  //   const { isOpen, onToggle } = useDisclosure()
-  //   const {searchTerm, setSearchTerm, searcOnNavOpen} = useContext(AuthContext);
-
-    
-  //   const handleClose = () => {
-  //     setSearchTerm(""); // Clear the search term
-  //     onToggle(); // Close the search bar
-  //   };
-
-  //   const topPosition = useBreakpointValue({ base: searcOnNavOpen ? '265px' : '120px', md: '120px' }); // Adjust the breakpoint and top values as needed
-
-
-  
-  //   return (
-  //     <>
-  //       <AiOutlineSearch color={"black"} size={25} onClick={onToggle} style={{ cursor: "pointer" }} />
-  //       <Collapse in={isOpen} style={{ position:'fixed', top: topPosition, left: '50%', transform: 'translate(-50%, -50%)', width:'50%'}}>
-  //           <InputGroup>
-  //           <Input
-  //             placeholder="Enter Something..."
-  //             _placeholder={{ color: "black" }}
-  //             onChange={(e) => setSearchTerm(e.target.value)}
-  //             value={searchTerm}
-  //             borderRadius={100}
-  //             backgroundColor={"white"}
-  //             _focusVisible={"none"}
-  //             borderColor={"gray.200"}
-  //           />
-  //             <InputLeftElement>
-  //               <SearchIcon />
-  //             </InputLeftElement>
-  //             <InputRightElement>
-  //               <AiOutlineClose color={"black"} size={20} onClick={handleClose} style={{ cursor: "pointer" }} />
-  //             </InputRightElement>
-  //           </InputGroup>
-  //       </Collapse>
-  //     </>
-  //   )
-  // }
-
 
     const SearchBar = () =>{
     const { isOpen, onOpen, onClose } = useDisclosure()
@@ -142,7 +56,6 @@
               <ProductCard 
                 key={product._id}
                 product={product}
-                //  addToCart={addToCart}
               />
             ))
           ) : (
